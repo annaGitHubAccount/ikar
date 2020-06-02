50903af00e43853ffe34a16141ccc2c17cc5556f
annaGitHubAccount <ann.polanska@gmail.com>
2020-06-02 11:16:57 +0200

    Ikar Config für WebService

--- src/main/java/de/anna/springboot/service/fabrik/ProduktStammdatenServiceFabrikWebServiceImpl.java
+++ src/main/java/de/anna/springboot/service/fabrik/ProduktStammdatenServiceFabrikWebServiceImpl.java
@@ -1,23 +1,40 @@
 package de.anna.springboot.service.fabrik;
 
 import de.anna.springboot.model.dto.ProduktStammdatenDTO;
-import de.anna.springboot.webservice.IkarWebServiceConfig;
-import de.anna.springboot.webservice.ProduktStammdatenRestClient;
+import de.anna.springboot.webservice.NestorWebServiceConfig;
+import de.anna.springboot.webservice.ProduktStammdatenListResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProduktStammdatenServiceFabrikWebServiceImpl implements ProduktStammdatenServiceFabrik{
+public class ProduktStammdatenServiceFabrikWebServiceImpl implements ProduktStammdatenServiceFabrik {
 
     @Autowired
-    private ProduktStammdatenRestClient produktStammdatenRestClient;
+    private NestorWebServiceConfig nestorWebServiceConfig;
 
-    @Override
+    private final RestTemplate restTemplate = new RestTemplate();
+
     public List<ProduktStammdatenDTO> findAll() {
 
-        List<ProduktStammdatenDTO> produktStammdatenDTOList = produktStammdatenRestClient.findAll();
+        List<ProduktStammdatenDTO> produktStammdatenDTOList = new ArrayList<>();
+
+        String urlVomNestor = gibURLVomNestorZurueck();
+
+        ProduktStammdatenListResponse response = restTemplate.getForObject(urlVomNestor + "/produktstammdaten", ProduktStammdatenListResponse.class);
+        if (response != null) {
+            produktStammdatenDTOList = response.getProduktStammdatenDTOList();
+        }
 
         return produktStammdatenDTOList;
+    }
+
+    private String gibURLVomNestorZurueck() {
+
+        String host = nestorWebServiceConfig.getHost();
+        String port = nestorWebServiceConfig.getPort();
+
+        return "http://" + host + ":" + port;
     }
 }
